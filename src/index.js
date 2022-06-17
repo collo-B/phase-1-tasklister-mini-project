39297365adb12171fd7c4c3ca40f6a9e631130ca
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-06-17 11:02:48 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let doc = document.getElementById('create-task-form')
+  doc.addEventListener('submit', function(event){
+    event.preventDefault()
+    p = document.createElement('p');
+    p.textContent = `${event.target['new-task-description'].value} `
+    button = document.createElement('button')
+    button.textContent = 'X'
+    doc.appendChild(p)
+    p.appendChild(button)
+
+    // delete
+    button.addEventListener('click', function(event){
+      event.target.parentNode.remove()
+    })
+   
+  })
 });
